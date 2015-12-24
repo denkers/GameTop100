6f9a90877f2fb4cda4f93f3818903fc8877a1642
denkers <kyle.business@inbox.com>
2015-12-24 15:43:41 +1300

    Updated site management with site list and site controls

--- app/views/user/sites.blade.php
+++ app/views/user/sites.blade.php
@@ -19,10 +19,33 @@
 	var site_fetch_url	=	'{{ URL::route("getMySiteList"); }}';
 </script>
 
-<div id='site_list_container'>
-	<pre>{{ print_r($site_list); }}</pre>
+<div id='site_list_container' class='container'>
+	<!-- SITE LIST -->
+	<ul class='list-group site_group'>
+		@foreach($site_list as $site)
+			<li class='list-group-item list_site_item clearifx'>
+				<div class='list_site_item_container'>
+					<!-- SITE DETAILS -->
+					<div class='site_item_details col-md-8'>
+						<h5 class='list_site_title'>{{ $site->title; }}
+							<br><small>{{ $site->description; }}</small>
+						</h5>					
+					</div>
 
-	<ul class='list-group'>
+					<!-- SITE CONTROLS -->
+					<div class='site_item_controls col-md-4'>
+						<!-- ADD SITE CONTROl -->
+						<a class='site_control_link' id='add_site_control' href='#'>
+							<span class='glyphicon glyphicon-plus'></span>
+						</a>
+	
+						<a class='site_control_link' id='remove_site_control' href='#'>
+							<span class='glyphicon glyphicon-remove'></span>
+						</a>
+					</div>
+				</div>
+			</li>
+		@endforeach
 	</ul>
 </div>
 
