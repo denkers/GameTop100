8535dda37d2672a952b1a88fb412e645a31cc932
denkers <kyle.business@inbox.com>
2015-12-25 23:51:25 +1300

    Updated user/sites with hiding of site view containers and displaying only on edit

--- public/javascript/user/site_management.js
+++ public/javascript/user/site_management.js
@@ -1,8 +1,18 @@
 $(function()
 {
+	$('.site_view_container').hide();
+
 	$('#remove_site_control').click(function(e)
 	{
 		e.preventDefault();
 		$('#remove_site_modal').modal('show');
+	});
+
+	$('#edit_site_control').click(function(e)
+	{
+		e.preventDefault();
+		var url	=	$(this).attr('href');
+
+		$('.site_view_container').slideDown('fast');
 	});
 });
