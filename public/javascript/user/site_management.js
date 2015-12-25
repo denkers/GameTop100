0b20f6ddf408e75ab4b28eae735c99356e202c66
denkers <kyle.business@inbox.com>
2015-12-25 23:59:11 +1300

    Updated editing site with displaying current site details

--- public/javascript/user/site_management.js
+++ public/javascript/user/site_management.js
@@ -13,6 +13,12 @@ $(function()
 		e.preventDefault();
 		var url	=	$(this).attr('href');
 
-		$('.site_view_container').slideDown('fast');
+		$.get(url, function(response)
+		{
+			$('.site_view_container').slideDown('fast');
+			$('input[name="s_desc"]').val(response.description);
+			$('input[name="s_title"]').val(response.title);
+			$('input[name="s_add"]').val(response.address);
+		});
 	});
 });
