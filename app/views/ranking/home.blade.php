1ef36808b81297fa8e1497994315e67f538785a2
denkers <kyle.business@inbox.com>
2015-12-31 22:45:25 +1300

    Removed laravel injected data in views/ranking

--- app/views/ranking/home.blade.php
+++ app/views/ranking/home.blade.php
@@ -23,7 +23,7 @@ Ranking
 <div data-ng-controller='rankingController'>
 	<div id='home_content'>
 		<div id='game_nav'>
-			<ul id='games_list' class='list-group'>
+			<ul id='games_list' class='list-group' data-ng-init='game_data = {{ GamesModel::getGames(); }}'>
 				<?php $game_list = GamesModel::getGames(); ?>
 				@if(isset($game_list))
 					@foreach($game_list as $game)
@@ -48,97 +48,90 @@ Ranking
 
 				<div class='ranking_container panel panel-default col-md-9'>
 					<div class='panel-body'>
-						<?php $ranking_list	=	SitesModel::getSitesForGame(1); ?>
 						<div class='list-group'>
-							@if(isset($ranking_list))
-								@foreach($ranking_list as $ranking_item)		
-									<?php $site_comments	=	SiteCommentsModel::getCommentsForSite($ranking_item['id']);  ?>
-									<div class='list-group-item clearfix ranking_item'>
-										<div class='rank_profile col-md-9'>
-											<!-- RANK NUMBER -->
-											<div class='col-md-2 rank_details'>
-												<h5 class='rank_display'>
-													1
-												</h5>
-											</div>
-		
-											<!-- SITE DETAILS -->
-											<div class='col-md-10 site_details'>
-												<!-- SITE IMAGE -->
-												<div class='site_profile_image'>
-
-												</div>
-
-												<!-- SITE DESCRIPTION -->
-												<div class='site_content'>
-													<a href='{{ $ranking_item["address"]; }}' class='site_link'>
-														{{ $ranking_item['title'] }}
-													</a>
-
-													<div class='site_description'>
-														{{ $ranking_item['description']; }}
-													</div>
-												</div>
-											</div>
+								<div class='list-group-item clearfix ranking_item' data-ng-repeat='ranking_item in ranking_list'>
+									<div class='rank_profile col-md-9'>
+										<!-- RANK NUMBER -->
+										<div class='col-md-2 rank_details'>
+											<h5 class='rank_display'>
+												1
+											</h5>
 										</div>
+	
+										<!-- SITE DETAILS -->
+										<div class='col-md-10 site_details'>
+											<!-- SITE IMAGE -->
+											<div class='site_profile_image'>
 
-										<div class='vote_details col-md-3'>
-											<h3 class='vote_text'>
-												<!-- SITE IN VOTES -->
-												<span class='site_in_group' data-toggle='tooltip' data-placement='bottom' data-title='Site in-votes'>
-													<span class='site_in'>5</span> <span class='glyphicon glyphicon-arrow-up'></span>
-												</span>
-
-												<!-- SITE OUT VOTES -->
-												<span class='site_out_group' data-toggle='tooltip' data-placement='bottom' data-title='Site out-votes'>
-													<span class='site_out'>10</span> <span class='glyphicon glyphicon-arrow-down'></span>
-												</span>
-
-												<!-- SITE COMMENTS -->
-												<a class='site_comments_group plain_link' href='{{ URL::route("getSiteComments"); }}' data-toggle='tooltip' data-placement='bottom' data-title='Site comments'>
-													<span class='site_comments'>{{ count($site_comments); }}</span> <span class='glyphicon glyphicon-comment'></span>
-												</a>
-											</h3>
-										</div> 
-										<div class='site_comments_container col-md-12'>
-											<div class='show_comments_container'>
-												<hr>
-												<span class='show_comments_msg'>Showing {{ count($site_comments)  }} comments</span>
 											</div>
-											@foreach($site_comments as $comment)
-												<br>
-												<div class='row comment_item'>
-													<div class='col-md-1'>
-														<a href='#' class='comment_writter'>{{ $comment->writter_id; }}</a>
-													</div>
 
-													<div class='col-md-6'>
-														<p class='comment_content'>{{ $comment->content; }}</p>
-													</div>
+											<!-- SITE DESCRIPTION -->
+											<div class='site_content'>
+												<a href='<% ranking_item.address %>' class='site_link'>
+													<% ranking_item.title %>
+												</a>
 
-													<div class='col-md-5'>
-														<div class='comment_rating_container col-md-6'>
-															<div class='col-md-1'>
-																<span class='comment_rating'>{{ ($comment->comment_rating > 0? '+' : '') . $comment->comment_rating; }}</span>
-															</div>
-															<div class='comment_rating_controls col-md-3'>
-																<span class='glyphicon glyphicon-chevron-up comment_control comment_upvote'></span>
-																<span class='glyphicon glyphicon-chevron-down comment_control comment_downvote'></span>
-															</div>	
-														</div>
-
-														<div class='comment_controls_container col-md-6'>
-															<span class='glyphicon glyphicon-pencil edit_comment_btn'></span>
-															<span class='glyphicon glyphicon-remove remove_comment_btn'></span>
-															<span class='glyphicon glyphicon-flag report_comment_btn'></span>
-														</div>
-													</div>
-												<div>
-											@endforeach	
+												<div class='site_description'>
+													<% ranking_item.description; %>
+												</div>
+											</div>
 										</div>
 									</div>
-								@endforeach
-							@endif
+
+									<div class='vote_details col-md-3'>
+										<h3 class='vote_text'>
+											<!-- SITE IN VOTES -->
+											<span class='site_in_group' data-toggle='tooltip' data-placement='bottom' data-title='Site in-votes'>
+												<span class='site_in'>5</span> <span class='glyphicon glyphicon-arrow-up'></span>
+											</span>
+
+											<!-- SITE OUT VOTES -->
+											<span class='site_out_group' data-toggle='tooltip' data-placement='bottom' data-title='Site out-votes'>
+												<span class='site_out'>10</span> <span class='glyphicon glyphicon-arrow-down'></span>
+											</span>
+
+											<!-- SITE COMMENTS -->
+											<a class='site_comments_group plain_link' href='{{ URL::route("getSiteComments"); }}' data-toggle='tooltip' data-placement='bottom' data-title='Site comments'>
+												<span class='site_comments'><% comment_list.length %></span> <span class='glyphicon glyphicon-comment'></span>
+											</a>
+										</h3>
+									</div> 
+									<div class='site_comments_container col-md-12'>
+										<div class='show_comments_container'>
+											<hr>
+											<span class='show_comments_msg'>Showing <% comment_list.length %> comments</span>
+										</div>
+											<div class='row comment_item' data-ng-repeat='comment in comment_list'>
+												<div class='col-md-1'>
+													<a href='#' class='comment_writter'><% comment.writter_id %></a>
+												</div>
+
+												<div class='col-md-6'>
+													<p class='comment_content'><% comment.content %></p>
+												</div>
+
+												<div class='col-md-5'>
+													<div class='comment_rating_container col-md-6'>
+														<div class='col-md-1'>
+															<span class='comment_rating'>
+																<% (comment.comment_rating > 0? '+' : '') + comment.comment_rating; %>
+															</span>
+														</div>
+														<div class='comment_rating_controls col-md-3'>
+															<span class='glyphicon glyphicon-chevron-up comment_control comment_upvote'></span>
+															<span class='glyphicon glyphicon-chevron-down comment_control comment_downvote'></span>
+														</div>	
+													</div>
+
+													<div class='comment_controls_container col-md-6'>
+														<span class='glyphicon glyphicon-pencil edit_comment_btn'></span>
+														<span class='glyphicon glyphicon-remove remove_comment_btn'></span>
+														<span class='glyphicon glyphicon-flag report_comment_btn'></span>
+													</div>
+												</div>
+											<div>
+									</div>
+									</div>
 						</div>
 					</div>
 				</div>
