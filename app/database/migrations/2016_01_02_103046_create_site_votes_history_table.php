9b457a6b7f502dac9b36e4da616a696f8e7c53f5
denkers <kyle.business@inbox.com>
2016-01-02 23:33:37 +1300

    Added site_votes_history table for storing historic monthly site data

---
+++ app/database/migrations/2016_01_02_103046_create_site_votes_history_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSiteVotesHistoryTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('site_votes_history', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('site_id')->unsigned();
+			$table->integer('votes_in');
+			$table->integer('votes_out');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('site_votes_history');
+	}
+
+}
