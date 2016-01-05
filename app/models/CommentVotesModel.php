87d26cba80f0fc5df2fffbc4050e3e2bc1e44bda
denkers <kyle.business@inbox.com>
2016-01-05 23:44:41 +1300

    Added models/CommentVotesModel

---
+++ app/models/CommentVotesModel.php
@@ -0,0 +1,12 @@
+<?php
+//==================================
+//	Kyle Russell
+//	github.com/denkers/GameTop100
+//	CommentVotesModel	
+//==================================
+
+
+class CommentVotesModel extends Eloquent
+{
+	protected $table	=	'comment_votes';
+}
