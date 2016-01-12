2020a051142e788a27543d0cc10d3fdf21837bad
denkers <kyle.business@inbox.com>
2016-01-12 23:10:52 +1300

    Added one-to-many relationship in SiteCommentsModel to CommentVotesModel

--- app/models/SiteCommentsModel.php
+++ app/models/SiteCommentsModel.php
@@ -9,8 +9,17 @@ class SiteCommentsModel extends Eloquent
 {
 	protected $table = 'site_comments';
 
+	public function userVotes()
+	{
+		return $this->hasMany('CommentVotesModel', 'comment_id');
+	}
+
 	public static function getCommentsForSite($site_id)
 	{
-		return self::where('site_id', '=', $site_id)->orderBy('created_at', 'desc')->get();
+		return self::where('site_id', '=', $site_id)->orderBy('created_at', 'desc')
+			->with(['userVotes', function($query)
+			{
+				$query->where('user_id', '=', Auth::user()->username);
+			}])->get();
 	}
 }
