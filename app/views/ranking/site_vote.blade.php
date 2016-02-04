e24436e5341586c2799461483572553c0632c436
denkers <kyle.business@inbox.com>
2016-02-04 16:08:07 +1300

    Updated css/master with center-x/center-y, updated layout in views/ranking/site_vote

--- app/views/ranking/site_vote.blade.php
+++ app/views/ranking/site_vote.blade.php
@@ -17,30 +17,35 @@
 @stop
 
 @section('content')
-	<div class='container' data-ng-controller='voteController'>
-		<div class='page-header'>
-			<h4>Site vote<br><small>Vote for your site</small></h4>
-		</div>
+	<div class='container' data-ng-controller='voteController' data-ng-init='site = {{ $site_data }}'>
+		<div class='col-md-9 center-x'>
+			<div class='page-header'>
+				<h4>Site vote<br><small>Vote for your site</small></h4>
+			</div>
 
-		<div class='row'>
-			<div class='col-md-12'>
-				<div class='list-group-item' data-ng-controller='siteController' data-ng-init='site = {{ $site_data }}'>
-					<site site-data='<% site %>' 
-						edit-url='{{ URL::route("postEditComment"); }}' 
-						add-url='{{ URL::route("postAddSiteComment"); }}' 
-						rate-url='{{ URL::route("postRateComment"); }}' 
-						delete-url='{{ URL::route("postRemoveComment"); }}'>
-					</site>
+			<div class='row'>
+				<div class='col-md-12'>
+					<div class='list-group-item' data-ng-controller='siteController'> 
+						<site site-data='<% site %>' 
+							edit-url='{{ URL::route("postEditComment"); }}' 
+							add-url='{{ URL::route("postAddSiteComment"); }}' 
+							rate-url='{{ URL::route("postRateComment"); }}' 
+							delete-url='{{ URL::route("postRemoveComment"); }}'>
+						</site>
+					</div>
 				</div>
-			</div>
 
-			<div class='col-md-4'>	
-				<div data-ng-model='voteData.vote_captcha' class="g-recaptcha" data-sitekey="<% $root.siteKey %>"></div>
-			</div>
 
-			<div class='col-md-8'>
-				<h4><span class='glyphicon glyphicon-info-sign'></span> By voting, you agree to the <a href='#'>terms of service</a></h4>
-				<button class='btn btn-lg btn-success col-md-3'><span class='glyphicon glyphicon-ok-sign'></span> Vote</button>
+				<div id='vote-container'>	
+					<div class='col-md-4'>	
+						<div data-ng-model='voteData.vote_captcha' class="g-recaptcha" data-sitekey="<% $root.siteKey %>"></div>
+					</div>
+
+					<div class='col-md-8'>
+						<h4><span class='glyphicon glyphicon-info-sign'></span> By voting, you agree to the <a href='#'>terms of service</a></h4>
+						<button class='btn btn-lg btn-success col-md-3'><span class='glyphicon glyphicon-ok-sign'></span> Vote</button>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
