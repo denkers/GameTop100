c306f1aa9764e1c7624d50633779cb7a30b01342
denkers <kyle.business@inbox.com>
2016-02-07 21:18:26 +1300

    Added models/NotificationsModel

---
+++ app/models/NotificationsModel.php
@@ -0,0 +1,12 @@
+<?php
+
+//==================================
+//	Kyle Russell
+//	github.com/denkers/GameTop100
+//	NotificationsModel
+//==================================
+
+class NotificationsModel extends Eloquent
+{
+	protected $table	=	'notifications';
+}
