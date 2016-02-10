44cf943b99133a28e78e5ea9af7f51e09d2fe13b
denkers <kyle.business@inbox.com>
2016-02-10 22:53:41 +1300

    Added footer lists for info, account, advertise lists

--- app/views/layout/footer.blade.php
+++ app/views/layout/footer.blade.php
@@ -1,1 +1,28 @@
-<h2>Hello</h2>
+<div class='row'>
+	<div class='col-md-4'>
+		<h4>Info</h4>
+		<div class='list-group footer-list'>
+			<a href='#' class='list-group-item'>About</a>
+			<a href='#' class='list-group-item'>Site map</a>
+			<a href='#' class='list-group-item'>Contact</a>
+		</div>
+	</div>
+
+	<div class='col-md-4'>
+		<h4>Account</h4>
+		<div class='list-group footer-list'>
+			<a href='#' class='list-group-item'>Register</a>
+			<a href='#' class='list-group-item'>Login</a>
+			<a href='#' class='list-group-item'>Forgot password</a>
+		</div>
+	</div>
+
+	<div class='col-md-4'>
+		<h4>Advertise</h4>
+		<div class='list-group footer-list'>
+			<a href='#' class='list-group-item'>Ranking</a>
+			<a href='#' class='list-group-item'>Premium</a>
+			<a href='#' class='list-group-item'>Ad Pricing</a>
+		</div>
+	</div>
+</div>
