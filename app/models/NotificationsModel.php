4df35db2a6e9142f75783f2d20c00f57aa04c778
denkers <kyle.business@inbox.com>
2016-02-11 23:34:09 +1300

    Added readNotification, getNumNotifications and getUserNotifications in models/NotificationsModel

--- app/models/NotificationsModel.php
+++ app/models/NotificationsModel.php
@@ -9,4 +9,27 @@
 class NotificationsModel extends Eloquent
 {
 	protected $table	=	'notifications';
+
+	public static function getUsersNotifications($user, $readNotifications)
+	{
+		return self::where('user', '=', $user)
+				->where('isRead', '=', $readNotifications)
+				->orderBy('created_at');
+	}
+
+	public static function getNumNotifications($user, $readNotifications)
+	{
+		return self::getUsersNotifications($user, $readNotifcations)->count();
+	}
+
+	public static function readNotification($notificationID)
+	{
+		$notification	=	NotificationsModel::find($notificationID);
+		if($notification == null) return false;
+		else
+		{
+			$notification->isRead	=	false;
+			return $notification->save();
+		}	
+	}
 }
