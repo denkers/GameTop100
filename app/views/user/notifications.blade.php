e58baf60c7e1f322f7daff42ed1c557995102190
denkers <kyle.business@inbox.com>
2016-02-13 19:29:44 +1300

    Added collapsable notification content and notification controls

--- app/views/user/notifications.blade.php
+++ app/views/user/notifications.blade.php
@@ -15,22 +15,35 @@
 			<div class='panel panel-default' data-ng-repeat='notification in notifications'>
 				<div class='panel-heading'>
 					<h3 class='panel-title'>
-						<span class='notification-subject'>
+						<span class='notification-subject notification-header'>
 							<span class='glyphicon glyphicon-bullhorn'></span> 
 							<% notification.subject %>
 						</span> 
 
-						<span class='notification-data'>
+						<span class='notification-data notification-header'>
 							<span class='glyphicon glyphicon-calendar'></span> 
 							<% notification.created_at %>
 						</span> 
 	
-						<span class='notification-read'>
+						<span class='notification-read notification-header'>
 							<span class='label label-<% notification.read? "default" : "success" %>'>
 								<% notification.read? "new" : "old" %>
 							</span>
 						</span>
 					</h3>
+				</div>
+
+				<div class='panel-body notification-body' uib-collapse='selectedNotification != $index'>
+					<div class='notification-content'>
+
+					</div>
+
+					<div class='notification-controls'>
+						<div class='btn-group'>
+							<button class='btn btn-default'><span class='glyphicon glyphicon-arrow-left'></span> Back</button>
+							<button class='btn btn-danger'><span class=' glyphicon glyphicon-remove'></span> Remove</button>	
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
