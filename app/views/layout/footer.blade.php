d7b96324a10a14cc5abfd781763189b5018167db
denkers <kyle.business@inbox.com>
2016-02-15 22:26:58 +1300

    Added subscribe controls for footer in views/layout/footer

--- app/views/layout/footer.blade.php
+++ app/views/layout/footer.blade.php
@@ -3,29 +3,34 @@
 		<h3><strong>Stay connected</strong></h3>
 		<br>
 
-		<!-- FACEBOOK MEDA -->
-		<div class='media-circle'>
-			<i class="fa fa-facebook"></i>
-		</div>
+		<div id='footer-media-links'>
+			<!-- FACEBOOK MEDA -->
+			<div class='media-circle'>
+				<i class="fa fa-facebook"></i>
+			</div>
 
-		<!-- TWITTER MEDIA -->
-		<div class='media-circle'>
-			<i class="fa fa-twitter"></i>
-		</div>
+			<!-- TWITTER MEDIA -->
+			<div class='media-circle'>
+				<i class="fa fa-twitter"></i>
+			</div>
 
-		<!-- GOOGLE+ MEDIA -->
-		<div class='media-circle'>
-			<i class="fa fa-google-plus"></i>			
-		</div>
+			<!-- GOOGLE+ MEDIA -->
+			<div class='media-circle'>
+				<i class="fa fa-google-plus"></i>			
+			</div>
 
-		<!-- RSS MEDIA -->
-		<div class='media-circle'>
-			<i class="fa fa-rss"></i>
+			<!-- RSS MEDIA -->
+			<div class='media-circle'>
+				<i class="fa fa-rss"></i>
+			</div>
 		</div>
 	</div>
 </div>
 
 <div class='footer-container col-md-6 center-x'>
+	<div id='footer-subscribe-container' class='row'>
+		<h2>Get the latest GameTop100 updates <button class='btn btn-lg btn-primary'>Subscribe</button></h2>
+	</div>
 	<div class='row'>
 		<div class='col-md-4'>
 			<h3 class='footer-heading'><span class='glyphicon glyphicon-info-sign'></span> Info</h4>
@@ -56,6 +61,6 @@
 	</div>
 
 	<div id='footer-copyright-container'>
-		<h4>Copyright</h4>
+		<h4><small>Copyright Kyle Russell 2016</small></h4>
 	</div>
 </div>
