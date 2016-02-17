8b6f60379366e7a3bcb1badd84eb4eb1257732ac
denkers <kyle.business@inbox.com>
2016-02-17 20:48:44 +1300

    Added login successful message in login modal on successful attempt in templates/user/login

--- public/templates/user/login.blade.php
+++ public/templates/user/login.blade.php
@@ -4,48 +4,57 @@
 </div>
 
 <div class='modal-body'>
-<!-- LOGIN STATUS ALERT -->
-<uib-alert type='<% loginResponse.status? "success" : "danger" %>' data-ng-show='loginResponse' close='loginResponse = null'>
-	<span class='<% loginResponse.status? "glyphicon glyphicon-ok-sign" : "glyphicon glyphicon-remove-sign" %>'></span> <% loginResponse.message %>
-</uib-alert>
-
-<!-- LOGIN FORM -->
-<form id='login_form'> 
-
-	<!-- USERNAME FIELD -->
-	<div class='form-group'>
-		<div class='input-group'>
-			<span class='input-group-addon'><span class='glyphicon glyphicon-user'></span></span>
-			<input type='text' id='username_field' class='form-control' 
-			name='login_id' placeholder='Enter your username'
-			data-ng-model='loginData.username'/>
-		</div>
-	</div>	
-
-	<!-- PASSWORD FIELD -->
-	<div class='form-group'>
-		<div class='input-group'>
-			<span class='input-group-addon'><span class='glyphicon glyphicon-lock'></span></span>
-			<input type='password' id='password_field' class='form-control'
-			name='login_pass' placeholder='Enter your password'
-			data-ng-model='loginData.password' />
-		</div>
+	<div id='login-success-container' data-ng-if='!loginResponse.status'>
+		<center>
+			<h2><span class='glyphicon glyphicon-ok-sign'></span> Login successful</h2>
+			<h2><small>One moment while we redirect you</small></h2>
+		</center>
 	</div>
 
-	<!-- REMEMBER FIELD -->
-	<div class='form-group'>
-		<div class='checkbox'>
-			<label><input type='checkbox' name='login_remember' data-ng-model='loginData.remember'>Remember me</label>
-		</div>
-	</div>
+	<div id='login-attempt-container' data-ng-if='loginResponse.status'>
+		<!-- LOGIN STATUS ALERT -->
+		<uib-alert type='<% loginResponse.status? "success" : "danger" %>' data-ng-show='loginResponse' close='loginResponse = null'>
+			<span class='<% loginResponse.status? "glyphicon glyphicon-ok-sign" : "glyphicon glyphicon-remove-sign" %>'></span> <% loginResponse.message %>
+		</uib-alert>
 
-	<div id='login_controls'>
-		<div data-ng-show='loginAttempts >= attemptLimit' vc-recaptcha key="$root.siteKey"></div><br>
-		<div class='btn-group'>
-			<button class='btn btn-default' id='forgot_btn'>Forgot password</button>
-			<button class='btn btn-primary' id='login_btn' data-ng-click='login(); $event.preventDefault();'>Login</button>
-		</div>
+		<!-- LOGIN FORM -->
+		<form id='login_form'> 
+
+			<!-- USERNAME FIELD -->
+			<div class='form-group'>
+				<div class='input-group'>
+					<span class='input-group-addon'><span class='glyphicon glyphicon-user'></span></span>
+					<input type='text' id='username_field' class='form-control' 
+					name='login_id' placeholder='Enter your username'
+					data-ng-model='loginData.username'/>
+				</div>
+			</div>	
+
+			<!-- PASSWORD FIELD -->
+			<div class='form-group'>
+				<div class='input-group'>
+					<span class='input-group-addon'><span class='glyphicon glyphicon-lock'></span></span>
+					<input type='password' id='password_field' class='form-control'
+					name='login_pass' placeholder='Enter your password'
+					data-ng-model='loginData.password' />
+				</div>
+			</div>
+
+			<!-- REMEMBER FIELD -->
+			<div class='form-group'>
+				<div class='checkbox'>
+					<label><input type='checkbox' name='login_remember' data-ng-model='loginData.remember'>Remember me</label>
+				</div>
+			</div>
+
+			<div id='login_controls'>
+				<div data-ng-show='loginAttempts >= attemptLimit' vc-recaptcha key="$root.siteKey"></div><br>
+				<div class='btn-group'>
+					<button class='btn btn-default' id='forgot_btn'>Forgot password</button>
+					<button class='btn btn-primary' id='login_btn' data-ng-click='login(); $event.preventDefault();'>Login</button>
+				</div>
+			</div>
+		</form>
 	</div>
-</form>
 </div>
 
