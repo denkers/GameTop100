2d7c3d028f2ba48bd880f582796825a6b873e412
denkers <kyle.business@inbox.com>
2016-02-19 22:16:33 +1300

    Added models/SiteTagsModel

---
+++ app/models/SiteTagsModel.php
@@ -0,0 +1,12 @@
+<?php
+//==================================
+//	Kyle Russell
+//	github.com/denkers/GameTop100
+//	SiteTagsModel
+//==================================
+
+
+class SiteTagsModel extends Eloquent
+{
+	protected $table	=	'site_tags';
+}
